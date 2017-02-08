5a1b7e82d12ca77d25cd5781ecd379287c0eed1e
Victor Zhong <viczhong@gmail.com>
2017-02-08 15:44:27 -0500

    Implemented experimental vote function

--- FirebaseAuthentication/FirebaseAuthentication/User.swift
+++ FirebaseAuthentication/FirebaseAuthentication/User.swift
@@ -56,7 +56,48 @@ enum VoteCasted {
 }
 
 class Voting {
-    func voted(for photo: Photo) {
+    func voted(for photoID: FIRDatabaseReference, upvoted: Bool) {
         
-    }
+        // [START photo_vote_transaction]
+        photoID.runTransactionBlock({ (currentData: FIRMutableData) -> FIRTransactionResult in
+            if var data = currentData.value as? [String : AnyObject] {
+                var votes = data["vote"] as! [String : Int]
+                var upvotes = votes["upvotes"]!
+                var downvotes = votes["downvotes"]!
+                
+                if upvoted {
+                    upvotes += 1
+                }
+                else {
+                    downvotes += 1
+                }
+                
+                votes["upvotes"] = upvotes
+                votes["downvotes"] = downvotes
+                
+                currentData.value = data
+             
+                return FIRTransactionResult.success(withValue: currentData)
+            }
+            return FIRTransactionResult.success(withValue: currentData)
+        }) { (error, committed, snapshot) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        // [END photo_vote_transaction]
+        
+        let currentUserString = (FIRAuth.auth()?.currentUser?.uid)!
+        let photoIDString = URL(string: photoID.description())!.lastPathComponent
+        
+        // [START vote_vote_transaction]
+           let databaseVoteReference = FIRDatabase.database().reference().child("votes")
+        databaseVoteReference.updateChildValues([currentUserString : upvoted])
+        // [END vote_vote_transaction]
+        
+        // [START user_vote_transaction]
+        let userVoteReference = FIRDatabase.database().reference().child("users").child(currentUserString).child("votes")
+        userVoteReference.updateChildValues([photoIDString : upvoted])
+        // [END user_vote_transaction]
+    } 
 }
